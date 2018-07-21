73aea3d1b0b8a2205575b2d16b7a25b9774c5c31
xztaityozx <taityonohibi@gmail.com>
2018-07-21 11:28:28 +0900

    add test

--- cmd/srun_test.go
+++ cmd/srun_test.go
@@ -2,28 +2,30 @@ package cmd
 
 import (
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 )
 
+var sim Simulation = Simulation{
+	DstDir: "/home/xztaityozx/WorkSpace/Test/",
+	SimDir: "/home/xztaityozx/WorkSpace/Test/",
+	Monte:  []string{"50000"},
+	Range:  Range{Start: "0.0", Step: "1.0", Stop: "2.0"},
+	Signal: "n1",
+	Vtn: Node{
+		Voltage:   0.6,
+		Sigma:     0.1,
+		Deviation: 1.0,
+	},
+	Vtp: Node{
+		Voltage:   -0.6,
+		Sigma:     0.1,
+		Deviation: 1.0,
+	},
+}
+
 func TestSetSEEDInputSPI(t *testing.T) {
-	sim := Simulation{
-		DstDir: "/home/xztaityozx/WorkSpace/Test/",
-		SimDir: "/home/xztaityozx/WorkSpace/Test/",
-		Monte:  []string{"50000"},
-		Range:  Range{Start: "0.0", Step: "1.0", Stop: "2.0"},
-		Signal: "n1",
-		Vtn: Node{
-			Voltage:   0.6,
-			Sigma:     0.1,
-			Deviation: 1.0,
-		},
-		Vtp: Node{
-			Voltage:   -0.6,
-			Sigma:     0.1,
-			Deviation: 1.0,
-		},
-	}
 
 	ConfigDir = "/home/xztaityozx/.config/UHA"
 
@@ -42,4 +44,34 @@ func TestSetSEEDInputSPI(t *testing.T) {
 		t.Fatal("Unexpected result : file name : ", f[0].Name())
 	}
 
+	os.Remove(p)
+}
+
+var nt NSeedTask = NSeedTask{
+	Simulation: sim,
+	Count:      3,
+}
+
+func TestSetResultDir(t *testing.T) {
+
+	if err := setResultDir(nt); err != nil {
+		t.Fatal(err)
+	}
+
+	f, _ := ioutil.ReadDir(nt.Simulation.DstDir)
+
+	if len(f) != 3 {
+		t.Fatal("Unexpected result len(f) : ", len(f))
+	}
+
+	for _, v := range f {
+		if !v.IsDir() || len(v.Name()) != len("Monte50000_SEEDx") {
+			t.Fatal("Unexpected result")
+		}
+	}
+}
+
+func TestMakeSRun(t *testing.T) {
+	expect := "cd /home/xztaityozx/WorkSpace/Test"
+	actual := makeSRun(nt)
 }
