49dfbd899dc070bb23ae076705b8a85467338399
xztaityozx <taityonohibi@gmail.com>
2018-07-31 16:49:34 +0900

    [ci skip] test

--- cmd/push.go
+++ cmd/push.go
@@ -105,7 +105,7 @@ func Push(pd *PushData) {
 	//Data:             data,
 	//}
 
-	fmt.Println(&data)
+	fmt.Println(&data[0])
 
 	//res, err := sheetService.Spreadsheets.Values.BatchUpdate(spreadsheetId, reqest).Context(ctx).Do()
 	//if err != nil {
